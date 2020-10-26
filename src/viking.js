d9a7dcb6cd0b1d91400738b1074074bfc48f016c
Franck SAMY <franck.samy@gmail.com>
2020-10-26 10:10:29 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,64 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    };
+    receiveDamage(damage) {
+        this.health -= 50;
+    };
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super();
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    battleCry() {
+        return "Odin Owns You All!"
+    };
+    receiveDamage(damage) {
+        this.health -= 50;
+        if (this.health == undefined) {
+            return ("`${name}` has died in act of combat");
+        }
+        if (this.health > 0) {
+            return ("`${name}` has received 50 points of damage");
+
+        }
+
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength)
+    }
+    receiveDamage(damage) {
+        this.health -= 50;
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = []
+    }
+    addViking(viking) {
+        return;
+    };
+
+    addSaxon(saxon) { }
+    vikingAttack() {
+        saxon[receiveDamage] = viking[strength];
+    }
+}
